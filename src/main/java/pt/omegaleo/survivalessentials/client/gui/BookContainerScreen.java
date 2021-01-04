b8bd744c2ffcabc39584f6ca7b5d071b28f65b17
Nuno Diogo <nunodiogo@omegaleo.pt>
2021-01-04 22:19:25 +0000

    Working on mod guide book

--- src/main/java/pt/omegaleo/survivalessentials/client/gui/BookContainerScreen.java
+++ src/main/java/pt/omegaleo/survivalessentials/client/gui/BookContainerScreen.java
@@ -7,12 +7,15 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import pt.omegaleo.survivalessentials.SurvivalEssentialsMod;
 import pt.omegaleo.survivalessentials.containers.BookContainer;
 import pt.omegaleo.survivalessentials.inventory.BackpackContainer;
@@ -23,7 +26,18 @@ public class BookContainerScreen extends ContainerScreen<BookContainer>
     private int currentPage = 0;
     private static final ResourceLocation[] PAGES = new ResourceLocation[]
     {
-        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page1.png")
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page1.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page2.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page3.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page4.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page5.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page6.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page7.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page8.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page9.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page10.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page11.png"),
+        new ResourceLocation(SurvivalEssentialsMod.MOD_ID,"textures/gui/pages/page12.png")
     };
 
     public BookContainerScreen(BookContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) 
@@ -68,7 +82,24 @@ public class BookContainerScreen extends ContainerScreen<BookContainer>
         int posY = (this.height - 256) / 2;
         // blit(posX, posY, minU, minV, maxU, maxV)
         blit(matrixStack, posX, posY, 0, 0, 256, 256);
-        minecraft.getTextureManager().bindTexture(PAGES[0]);
+        minecraft.getTextureManager().bindTexture(PAGES[currentPage]);
         blit(matrixStack, posX, posY, 0, 0, 256, 256);
+
+        Button prevPage = new Button(posX - 64, posY + 64, 16, 16, new TranslationTextComponent("gui.book.prev"), (button) -> {
+            if(currentPage > 0)
+            {
+                currentPage--;
+            }
+        });
+        addButton(prevPage);
+
+        Button nextPage = new Button(posX + 256 + 64, posY + 64, 16, 16, new TranslationTextComponent("gui.book.next"), (button) -> {
+            if(currentPage < PAGES.length-1)
+            {
+                currentPage++;
+            }
+        });
+        addButton(nextPage);
+        
     }
 }
